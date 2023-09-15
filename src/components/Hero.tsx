b702a4926831d81da0464b56f36ca031cd3b4dfc
Jesus Borjas <jesusbor006@gmail.com>
2023-09-15 12:11:51 -0500

    Create a responsive hero section

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,15 +1,36 @@
+import {
+  BsLinkedin,
+  BsGithub,
+  BsFillFileEarmarkTextFill,
+} from "react-icons/bs";
+
 const Hero = () => {
   return (
-    <>
-      <h2>Hi! I'm Jesus Borjas.</h2>
-      <h2>A Front-End Developer</h2>
-      <p>
+    <section className="text-center px-6 mx-auto mt-24">
+      <h2 className="text-3xl font-semibold md:text-4xl lg:text-5xl">
+        Hi! I'm Jesus Borjas.
+      </h2>
+      <h2 className="text-3xl font-semibold mb-4 md:text-4xl lg:text-5xl">
+        A Front-End Developer
+      </h2>
+      <p className="max-w-[550px] mx-auto md:text-lg ">
         I have a strong passion for building a responsive and performant web
         application with great user experiences that positively impacts the
         world. I am always striving to learn and develop new skills and become a
         better developer.
       </p>
-    </>
+      <div className="flex justify-center mt-4 space-x-6">
+        <a href="http://www.google.com" target="_blank" rel="noreferrer">
+          <BsLinkedin size={30} />
+        </a>
+        <a href="http://www.google.com" target="_blank" rel="noreferrer">
+          <BsGithub size={30} />
+        </a>
+        <a href="http://www.google.com" target="_blank" rel="noreferrer">
+          <BsFillFileEarmarkTextFill size={30} />
+        </a>
+      </div>
+    </section>
   );
 };
 
